dee958d9e8beb0bfb4e870d3a9ed33cec8fe97fd
willcruz07 <lynkan@hotmail.com>
2024-06-29 08:40:48 -0300

    fix: main container

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 export function MainContainer({ children }: React.PropsWithChildren) {
   return (
-    <main className="flex h-screen w-screen items-center justify-center">
-      <div className="flex min-h-screen min-w-full max-w-[932px] flex-col bg-gray-950 p-5">
-        {children}
-      </div>
+    <main className="flex min-h-screen max-w-2xl flex-col bg-gray-950 p-5">
+      {children}
     </main>
   );
 }

c57bfd61c72a37eb0a102c602e3106227252406c
willcruz07 <lynkan@hotmail.com>
2024-06-29 00:14:33 -0300

    test key

--- src/service/firebase/configServer.ts
+++ src/service/firebase/configServer.ts
@@ -4,7 +4,10 @@ const firebaseAdmin = !admin.apps.length
   ? admin.initializeApp({
       credential: admin.credential.cert({
         clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(
+          /\\n/g,
+          '\n'
+        ),
         projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
       }),
     })
